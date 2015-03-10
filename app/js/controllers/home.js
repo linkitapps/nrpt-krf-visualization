579d83d7d3bb2ad8ebd60de6bf27aa6e154b91cb
linkitapps <master@linkit.kr>
2015-03-10 14:57:55 +0900

    네트워크 맵 생성

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -105,10 +105,210 @@ function HomeCtrl($q, KrfService, AppSettings) {
         var graphData = {};
         graphData.orgs = ret;
 
-        console.log(graphData);
+        // ajaxSpinner 지우기
+        var spinner = document.getElementById('ajaxSpinner');
+        spinner.parentNode.removeChild(spinner);
+
+        // 차트 생성
+        drawChart(graphData);
       }, 3000);
 
     }, 3000);
+  }
+
+  // 차트 생성 함수
+  function drawChart(data) {
+    var netChartData = {
+      nodes: [],
+      links: []
+    };
+
+    // 데이터 생성 시작
+    netChartData.nodes.push({
+      id: 'root',
+      loaded: true,
+      style: {
+        label: '고려대학교 공동기기'
+      }
+    });
+
+    _.each(data.orgs, function(org, i) {
+      netChartData.nodes.push({
+        id: 'org' + i,
+        loaded: true,
+        style: {
+          label: org.name
+        }
+      });
+
+      _.each(org.categories, function(cat, j) {
+        netChartData.nodes.push({
+          id: 'org' + i + '_cat' + j,
+          loaded: true,
+          style: {
+            label: cat.name
+          }
+        });
+
+        _.each(cat.equipments, function(equ, k) {
+          netChartData.nodes.push({
+            id: 'org' + i + '_cat' + j + '_equ' + k,
+            loaded: true,
+            style: {
+              label: equ.nequ050_artl_nm
+            }
+          })
+        });
+      });
+    });
+
+    _.each(netChartData.nodes, function(obj, i) {
+      var temp = obj.id.split('_');
+
+      if (temp.length == 1) {
+        netChartData.links.push({
+          id: 'link' + i,
+          from: 'root',
+          to: obj.id,
+          style: {
+            fillColor: 'red',
+            toDecoration: 'arrow'
+          }
+        });
+      } else if (temp.length == 2) {
+        netChartData.links.push({
+          id: 'link' + i,
+          from: temp[0],
+          to: obj.id,
+          style: {
+            fillColor: 'blue',
+            toDecoration: 'arrow'
+          }
+        });
+      } else if (temp.length == 3) {
+        netChartData.links.push({
+          id: 'link' + i,
+          from: temp[0] + '_' + temp[1],
+          to: obj.id,
+          style: {
+            fillColor: 'green',
+            toDecoration: 'arrow'
+          }
+        });
+      }
+    });
+    // 데이터 생성 끝
+
+    var startNodeId = "root";
+    var endNodeId = null;
+    var endpointColor = "#09c";
+    var pathColor = "#09c";
+    var pathIds = {};
+    var chart = 0;
+
+    // 차트 생성
+    chart = new NetChart({
+      container: document.getElementById('chartContainer'),
+      data: {
+        preloaded: netChartData
+      },
+      navigation:{
+        mode: 'focusnodes',
+        initialNodes: ['root']
+      },
+      style:{
+        makeImagesCircular: true,
+        nodeRules: {
+          '1-basic': nodeStyle,
+          '2-highlight': nodeHighlight
+        },
+        linkRules:{
+          '2-highlight':linkHighlight
+        }
+      },
+      toolbar: {
+        items: []
+      },
+      events:{
+        onHoverChange: function(event){
+          if (event.hoverNode)changeTargetNode(event.hoverNode)
+        },
+        onClick: function(event){
+          if (event.clickNode)changeTargetNode(event.clickNode)
+        }
+      }
+    });
+
+    function changeTargetNode(node){
+      var nodeId = node.id;
+      if (nodeId != endNodeId)
+      {
+        endNodeId = nodeId;
+        if (endNodeId) {
+          pathIds = computePath();
+        } else {
+          pathIds = {};
+        }
+
+        chart.updateStyle();
+      }
+    }
+
+    function nodeStyle(node){
+      node.labelStyle.textStyle.font = '14px dotum';
+    }
+
+    function nodeHighlight(node){
+      var endNode = (node.id == startNodeId || node.id == endNodeId);
+
+      if (endNode || pathIds[node.id]){
+        node.fillColor = pathColor;
+        node.label = node.data.style.label;
+      }
+
+      if (endNode){
+        node.lineColor = endpointColor;
+        node.lineWidth = 7;
+        node.radius = 50;
+      }
+    }
+
+    function linkHighlight(link){
+      if (pathIds[link.id]){
+        link.fillColor = pathColor;
+        link.radius = 4;
+      }
+    }
+
+    function computePath(){
+      //compute shortest path, quick and dirty
+      var from = chart.getNode(startNodeId);
+      var to = chart.getNode(endNodeId);
+      var back = {};
+      var queue = [from];
+      var link, next, cur;
+
+      while (queue.length > 0){
+        cur = queue.shift();
+        if (cur == to) break;
+        for (var i in cur.links){
+          link = cur.links[i];
+          next = link.otherEnd(cur);
+          if (back[next.id]) continue;
+          back[next.id] = link;
+          queue.push(next);
+        }
+      }
+
+      var result = {};
+      while (cur != from){
+        link = back[cur.id];
+        cur = link.otherEnd(cur);
+        result[link.id] = true;
+        result[cur.id] = true;
+      }
+      return result;
+    }
   }
 
 }
--- gulp/tasks/vendor.js
+++ gulp/tasks/vendor.js
@@ -8,6 +8,18 @@ var stripDebug   = require('gulp-strip-debug');
 
 gulp.task('vendor', function () {
   gulp.src([
+    'app/vendor/zoomcharts/assets/base.css',
+    'app/vendor/zoomcharts/assets/netchart.css'
+  ]).pipe(gulp.dest('build/'));
+
+  gulp.src([
+    'app/vendor/zoomcharts/assets/netchart-sprite.png',
+    'app/vendor/zoomcharts/assets/sprite.png',
+    'app/vendor/zoomcharts/assets/vertical-resizer.png',
+    'app/vendor/zoomcharts/assets/ajax_loader_blue_48.gif'
+  ]).pipe(gulp.dest('build/'));
+
+  gulp.src([
     'app/vendor/zoomcharts/zoomcharts.js' ])
     .pipe(stripDebug())
     .pipe(concatFiles('vendor.js'))
